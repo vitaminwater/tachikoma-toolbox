ebd05da912b21c4d6ad0bb8412980592c7e3decb
PJ Bergeron <pierrejeanbergeron@gmail.com>
2018-01-22 15:05:01 +0100

    Fix unmap

--- mapstructure.go
+++ mapstructure.go
@@ -5,7 +5,8 @@ import (
 )
 
 func Unmap(v map[string]interface{}, t interface{}) {
-	config := mapstructure.DecoderConfig{WeaklyTypedInput: true, Result: &t}
+	config := mapstructure.DecoderConfig{WeaklyTypedInput: true, Result: t}
+
 	decoder, err := mapstructure.NewDecoder(&config)
 	Fatal(err)
 
